b85bc22347f79b8d2f00ed17b934bde7a865d763
Have Samuel <samiehave9@gmail.com>
2022-10-14 02:46:38 +0300

    Removing the CSS classes using JS classes

--- sharad.js
+++ sharad.js
@@ -1,10 +1,37 @@
 let planButtons = document.querySelectorAll('.plan button');
 let modal = document.querySelector('.modal')
 let popup = document.querySelector('.backdrop');
+let modalNoButton = document.querySelector('.modal__action--negative');
+let toggleButton =document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav')
 
-for(let i = 0; i < planButtons.length; i++) {
-  document.addEventListener('click', function() {
-    popup.style.display = 'block';
-    modal.style.display = 'block';
-  })
+for (let i = 0; i < planButtons.length; i++) {
+  planButtons[i].addEventListener('click', function() {
+    // popup.style.display = 'block';
+    // modal.style.display = 'block';
+    modal.classList.add('open');
+    popup.classList.add('open')
+  });
 }
+
+popup.addEventListener('click', function() {
+  // mobileNav.style.display = 'none'; 
+  mobileNav.classList.remove('open');
+  closeModal();
+});
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+  // popup.style.display = 'none';
+  // modal.style.display = 'none';
+  popup.classList.remove('open');
+  modal.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function() {
+  // mobileNav.style.display = 'block';
+  // popup.style.display = 'block';
+  mobileNav.classList.add('open');
+  popup.classList.add('open');
+});
